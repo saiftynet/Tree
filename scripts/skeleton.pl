c9dd673dbc4bb288a37c357f5c941f92e669b9e4
Saif Ahmed <34284663+saiftynet@users.noreply.github.com>
2026-02-08 12:05:44 +0000

    Add files via upload

--- scripts/skeleton.pl
+++ scripts/skeleton.pl
@@ -3,7 +3,7 @@ use Node;
 my $skeleton=new Node ("skeleton");
 
 $skeleton->addChild("head","right arm","left arm","trunk","right leg","left leg");
-my $temp=$skeleton->child($skeleton->childByName("right arm"));
+my $temp=new Node("limb"); #make a generic limb
 $temp->addChild(qw/humerus radius ulna scaphoid
                 lunate triquetral pisiform trapezium
                 trapezoid capitate hamate/);
@@ -66,27 +66,35 @@ $temp->addChild(Node::deserialise("
 $temp->group("hand",   $temp->childrenByNames(qw/carpals
                 metacarpals digits/  ));  
                 
-$temp=Node::deserialise($temp->serialise());
+my $limb=$temp->serialise(); 
+
 foreach ($temp->children()){
-  $skeleton->child($skeleton->childByName("left arm"))->addChild($temp->child($_));
+  $skeleton->child("right arm")->addChild($temp->child($_));
+}
+
+$temp=Node::deserialise($limb); 
+foreach ($temp->children()){
+  $skeleton->child("left arm")->addChild($temp->child($_));
 }
 
 my %analogues=(qw/humerus femur radius tibia ulna fibula scaphoid talus
-              lunate calcaneus triquetral cuboid trapezium cuneiform1
+              lunate calcaneus triquetral cuboid pisiform navicular trapezium cuneiform1
               trapezoid cuneiform2 capitate cuneiform3 hand foot carpal tarsal
               thumb big_toe index 2nd_toe  long 3rd_toe ring 4th_toe little_toe/);
-my $limb=$temp->serialise();
-
+$limb=~s/\[hamate\]\n//g;
 foreach my $k (keys %analogues){
   $limb=~s/$k/$analogues{$k}/g if $analogues{$k}
-};
+}
+
 $temp=Node::deserialise($limb);
-$temp2=Node::deserialise($limb);
 foreach ($temp->children()){
-  $skeleton->child($skeleton->childByName("right leg"))->addChild($temp->child($_));
+  $skeleton->child("right leg")->addChild($temp->child($_));
 }
-foreach ($temp2->children()){
-  $skeleton->child($skeleton->childByName("left leg"))->addChild($temp2->child($_));
+
+$temp=Node::deserialise($limb);
+foreach ($temp->children()){
+  $skeleton->child("left leg")->addChild($temp->child($_));
 }
+
 $skeleton->setPaths();
 $skeleton->drawTree("p");
